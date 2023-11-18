16ed5708caf622186c9d386d288397c455ffe0fa
itai lupo <itailupo@gmail.com>
2023-11-18 04:33:33 +0200

    small cleanup

--- src/a.cpp
+++
@@ -1,4 +0,0 @@
-#include "a.hpp"
-#include "core.hpp"
-
-int a() { return 0; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
-#include <sys/cdefs.h>
-
 #include "core.hpp"
 
-#include "a.hpp"
-
 int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-    std::cout << "hello world" << std::endl;
-    spdlog::info("test");
+    spdlog::info("hello world");
 
     
-    SPDLOG_TRACE("Some trace message with param {}", 42);
-    SPDLOG_DEBUG("Some debug message");
-    spdlog::info("d");
     return 0;
 }
