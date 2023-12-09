433f7cd3c532ec671feca8046189fba3101678fa
itai <itailupo@gmail.com>
2023-12-10 01:51:56 +0200

    something with the submoudels

--- tests/main.test.cpp
+++ tests/main.test.cpp
@@ -12,7 +12,7 @@
 		x;                                                                                                             \
 		clock_gettime(CLOCK_REALTIME, &end);                                                                           \
 		double f = ((double)end.tv_sec * 1e9 + end.tv_nsec) - ((double)start.tv_sec * 1e9 + start.tv_nsec);            \
-		LOG_INFO("time {} ms\n", f / 1000000);                                                                         \
+		LOG_INFO("time {} ms", f / 1000000);                                                                         \
 	}
 
 int main(int argc, char **argv)
